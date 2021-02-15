c8484ef7c5ea2441a09da64ba8efbb9cebf63b4c
Javernaut <javernaut@gmail.com>
2021-02-15 11:43:46 +0200

    Updating CMake 3.10.2.4988404 -> 3.18.1

--- scripts/export-host-variables.sh
+++ scripts/export-host-variables.sh
@@ -26,7 +26,7 @@ export HOST_TAG=$HOST_TAG
 export HOST_NPROC=$HOST_NPROC
 
 # Using CMake from the Android SDK
-export CMAKE_EXECUTABLE=${ANDROID_SDK_HOME}/cmake/3.10.2.4988404/bin/cmake
+export CMAKE_EXECUTABLE=${ANDROID_SDK_HOME}/cmake/3.18.1/bin/cmake
 # Using Build machine's Make, because Android NDK's Make (before r21) doesn't work properly in MSYS2 on Windows
 export MAKE_EXECUTABLE=$(which make)
 # Using Build machine's Ninja. It is used for libdav1d building. Needs to be installed
